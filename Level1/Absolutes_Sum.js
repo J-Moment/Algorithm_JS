a1bc95d9f119b12a7b28117a18733e4b393584f0
SongJunHyuk <83599564+SongJunHyuk@users.noreply.github.com>
2022-09-27 21:40:44 +0900

    Absolutes_Sum

---
+++ Level1/Absolutes_Sum.js
@@ -0,0 +1,9 @@
+function solution(absolutes, signs) {
+    var answer = 0;
+    for(let i = 0 ; i < signs.length ; i++) {
+        if(signs[i] === false)
+            absolutes[i] *= -1;
+        answer += absolutes[i];
+    }
+    return answer;
+}
