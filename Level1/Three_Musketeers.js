4d2579d6190b97ca30fcaeb2573d6636cfdbbc26
SongJunHyuk <83599564+SongJunHyuk@users.noreply.github.com>
2022-10-23 19:11:06 +0900

    Three_Musketeers

--- Level1/Three_Musketeers.js
+++ Level1/Three_Musketeers.js
@@ -1,13 +1,4 @@
-function solution(number) {
-    var answer = 0;
-    for(var i=0; i<number.length; i++){
-        for(var j=i+1; j<number.length; j++){
-            for(var k=j+1; k<number.length; k++){
-                if(number[i] + number[j] + number[k] === 0){
-                    answer += 1;
-                }
-            }
-        }
-    }
-    return answer;
+function solution(n) {
+    let reversed_N = n.toString(3).split('').reverse().join('')
+    return parseInt(reversed_N, 3)
 }
