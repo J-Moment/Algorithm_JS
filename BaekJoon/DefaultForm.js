d4706370c60fbe1e1ca7b6da5d7506b8dc8f20c3
SongJunHyuk <83599564+SongJunHyuk@users.noreply.github.com>
2023-08-09 22:56:37 +0900

    DefaultForm

---
+++ BaekJoon/DefaultForm.js
@@ -0,0 +1,68 @@
+// readline 모듈 사용
+// ** 한 줄 입력 **
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.on('line', function(line) {
+  console.log(line);
+
+  rl.close();
+}).on("close", function() {
+  process.exit();
+});
+
+
+// ** 여러 줄 입력 **
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+  input.push(line)
+})
+  .on('close', function () {
+  console.log(input);
+  process.exit();
+});
+
+
+// fs 모듈 사용
+
+// ** 한 줄 입력 **
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split(' ');
+
+let num = Number(input);
+
+for (let i = 1; i <= num; i++) {
+  console.log(i);
+}
+
+// ** 여러 줄 입력 **
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let count = input[0];
+let numbers = [];
+
+for (let i = 1; i < input.length; i++) {
+  if (input[i] !== '') {
+    numbers.push(input[i].split(' '));
+  }
+}
+
+for (let i = 0; i < numbers.length; i++){
+  let num1 = Number(numbers[i][0]);
+  let num2 = Number(numbers[i][1]);
+
+  console.log(num1 + num2);
+}
