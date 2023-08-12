37fba658755a78f1f975c4a9e2631c6a9dfc76c2
SongJunHyuk <83599564+SongJunHyuk@users.noreply.github.com>
2023-08-12 22:45:45 +0900

    AxB

---
+++ BaekJoon/Stage1/AxB.js
@@ -0,0 +1,19 @@
+function solution(a , b){
+    console.log(a*b);
+}
+
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+let input = [];
+rl.on("line", function(line){
+    input = line.split(' ').map((el) => parseInt(el));
+    rl.close();
+}).on("close", function(){
+    let A = input[0];
+    let B = input[1];
+    solution(A, B);
+    process.exit();
+})
