bc66596fc5d05b949f5a2ae939b7c0d2a6455eb7
SongJunHyuk <83599564+SongJunHyuk@users.noreply.github.com>
2023-08-29 17:13:39 +0900

    Multiply

--- BaekJoon/Stage1/Multiply.js
+++ BaekJoon/Stage1/Multiply.js
@@ -1,3 +1,13 @@
+function solution (first, second) {
+    const oneNum = second % 10;
+    const tenNum = Math.floor((second % 100)/10);
+    const hundredNum = Math.floor(second / 100);
+    console.log(first * oneNum);
+    console.log(first * tenNum);
+    console.log(first * hundredNum);
+    console.log(first * second);
+}
+
 const readline = require('readline');
 
 const rl = readline.createInterface({
@@ -8,8 +18,11 @@ const rl = readline.createInterface({
 let input = [];
 
 rl.on('line', function (line) {
-  input.push(line)
+    input.push(line);
 })
   .on('close', function () {
+    const first = parseInt(input[0]);
+    const second = parseInt(input[1]);
+    solution(first, second);
   process.exit();
 });
