333c540872c137f2c89bfbf758c1d788b1258d26
SongJunHyuk <83599564+SongJunHyuk@users.noreply.github.com>
2023-09-04 17:27:44 +0900

    A+B-7

---
+++ BaekJoon/Stage3/A+B-7.js
@@ -0,0 +1,23 @@
+function solution(num) {
+    for (let i = 1; i <= num; i++) {
+        let a = parseInt(input[i].split(' ')[0]);
+        let b = parseInt(input[i].split(' ')[1]);
+
+        console.log(`Case #${i}: ${a + b}`);
+    }
+}
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let input = [];
+rl.on('line', function (line) {
+    input.push(line);
+}).on('close', function () {
+    let num = parseInt(input[0]);
+    solution(num);
+    process.exit();
+});
