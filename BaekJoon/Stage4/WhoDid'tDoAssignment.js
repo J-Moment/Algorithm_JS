2d89222004eb9a113bad49a225de315d02bbaa53
SongJunHyuk <83599564+SongJunHyuk@users.noreply.github.com>
2023-09-09 23:37:59 +0900

    WhoDid'tDoAssignment

---
+++ BaekJoon/Stage4/WhoDid'tDoAssignment.js
@@ -0,0 +1,26 @@
+function solution() {
+	let arr = [];
+	for (let i = 1; i <= 30; i++) {
+		if (!input.includes(i)) arr.push(i);
+		if (arr.length === 2) break;
+	}
+	console.log(arr.join("\n"));
+	process.exit();
+}
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  
+  let input = [];
+  
+  rl.on('line', function (line) {
+        input.push(+line);
+  })
+    .on('close', function () {
+        solution();
+    process.exit();
+});
