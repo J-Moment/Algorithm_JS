97bce1953380ce2b33332d0e5081aa966e069c5b
SongJunHyuk <83599564+SongJunHyuk@users.noreply.github.com>
2023-09-15 13:23:30 +0900

    Sprout

---
+++ BaekJoon/Stage6/Sprout.js
@@ -0,0 +1,24 @@
+function solution() {
+    console.log(
+`         ,r'"7
+r\`-_   ,'  ,/
+ \\\. ". L_r'
+   \`~\\/
+      |
+      |`
+        );
+}
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.on('line', function(line) {
+  rl.close();
+}).on("close", function() {
+    solution();
+  process.exit();
+});
