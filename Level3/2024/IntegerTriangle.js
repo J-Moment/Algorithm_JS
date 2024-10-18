8467b6eb1f53ae5bfd26d22651af168031bf3f67
SongJunHyuk <83599564+FE-Nerdy@users.noreply.github.com>
2024-10-19 08:09:16 +0900

    IntegerTriangle_SemicolonAdd

--- Level3/2024/IntegerTriangle.js
+++ Level3/2024/IntegerTriangle.js
@@ -7,10 +7,10 @@ function solution(triangle) {
     for (let i = height - 2; i >=0; i--) {
         for(let j = 0; j <= i; j++){
             memo[i][j] = triangle[i][j] + Math.max(memo[i + 1][j],
-                                                   memo[i + 1][j + 1])
+                                                   memo[i + 1][j + 1]);
         }
     }
 
-    const answer = memo[0][0]
-   return answer
+    const answer = memo[0][0];
+   return answer;
 }
