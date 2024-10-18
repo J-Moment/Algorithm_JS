3b017e37acc8ee66ea9118ae552f7db369bcf3d7
SongJunHyuk <83599564+FE-Nerdy@users.noreply.github.com>
2024-10-18 23:43:53 +0900

    IntegerTriangle

---
+++ Level3/2024/IntegerTriangle.js
@@ -0,0 +1,16 @@
+function solution(triangle) {
+    const height = triangle.length;
+    const memo = [...Array(height)].map((x,i) => [...Array(i + 1)].fill(0));
+    
+    triangle[height -1].forEach((value, index) => memo[height -1][index] = value);
+
+    for (let i = height - 2; i >=0; i--) {
+        for(let j = 0; j <= i; j++){
+            memo[i][j] = triangle[i][j] + Math.max(memo[i + 1][j],
+                                                   memo[i + 1][j + 1])
+        }
+    }
+
+    const answer = memo[0][0]
+   return answer
+}
