3ad1b75f725b898cfaf865262b6b9b472c9236ec
SongJunHyuk <83599564+FE-Nerdy@users.noreply.github.com>
2024-12-11 18:10:19 +0900

    DP

---
+++ BaekJoon/DP1/Lis.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const filepath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filepath).toString().split("\n");
+
+let N = +input[0];
+let li = input[1].split(" ").map(Number);
+
+const dp = new Array(N);
+
+for (let i = 0; i < N; i++) {
+  let max = 0;
+  for (let j = 0; j <= i; j++) {
+    if (li[j] < li[i]) max = Math.max(max, dp[j]);
+  }
+  dp[i] = max + 1;
+}
+
+console.log(Math.max(...dp));
