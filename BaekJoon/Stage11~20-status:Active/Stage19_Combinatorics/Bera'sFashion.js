b7d2adc2ac9fc907bb58cd53c620d2be923b703a
SongJunHyuk <83599564+FE-Nerdy@users.noreply.github.com>
2025-01-31 00:18:21 +0900

    Feat: solve "베라의 패션" problem on BaekJoon
    숫자 N에 대하여 서로 다른 두 개를 뽑는 모든 조합의 경우의 수 구현

---
+++ BaekJoon/Stage11~20-status:Active/Stage19_Combinatorics/Bera'sFashion.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+const filepath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filepath).toString().trim().split("\n");
+
+let N = Number(input[0]);
+
+console.log(N*(N-1));
