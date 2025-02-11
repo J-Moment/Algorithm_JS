6ee86203ae7ea2979db81cbac5aa2256705bc674
SongJunHyuk <83599564+FE-Nerdy@users.noreply.github.com>
2025-02-11 23:16:37 +0900

    Feat: solve "나무 자르기" problem on BaekJoon
    이분탐색을 활용하여 절단기 높이 중최댓값 구현

---
+++ BaekJoon/Stage21~30-status:Active/Stage27_BinarySearch/CutTrees.js
@@ -0,0 +1,30 @@
+let fs = require('fs');
+const filepath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filepath).toString().trim().split("\n");
+
+let [N, M] = input[0].split(" ").map(Number);
+let trees = input[1].split(" ").map(Number);
+
+let start = 0;
+let end = Math.max(...trees);
+
+let result = 0;
+while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+    let wood = 0;
+
+    for (let tree of trees) {
+        if (tree > mid) {
+            wood += tree - mid;
+        }
+    }
+
+    if (wood >= M) { 
+        result = mid;
+        start = mid + 1;
+    } else {
+        end = mid - 1;
+    }
+}
+
+console.log(result);
