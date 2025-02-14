11f63a3e6d99c03d27b5930d6350851479578f30
SongJunHyuk <83599564+FE-Nerdy@users.noreply.github.com>
2025-02-14 23:55:52 +0900

    Feat: solve "가장 긴 증가하는 부분 수열 2" problem on BaekJoon 
    이분탐색으로 가장 긴 증가하는 부분 수열 구현

--- BaekJoon/Stage21~30-status:Active/Stage27_BinarySearch/LIS2.js
+++ BaekJoon/Stage21~30-status:Active/Stage27_BinarySearch/LIS2.js
@@ -2,38 +2,35 @@ let fs = require("fs");
 const filepath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filepath).toString().trim().split("\n");
 
-let [N] = input[0];
-let li = input[1];
+let N = Number(input[0]);
+let li = input[1].split(" ").map(Number);
 
-
-function binarySearch(left, right, target) {
+function binarySearch(arr, left, right, target) {
   let mid;
   while (left < right) {
     mid = Math.floor((left + right) / 2);
 
-    if (lis[mid] < target) {
+    if (arr[mid] < target) {
       left = mid + 1;
     } else {
-      right = mid
+      right = mid;
     }
   }
   return right;
 }
 
 let lis = [];
-
 let j = 0;
 lis[0] = li[0];
 let i = 1;
 
 while (i < N) {
-
   if (lis[j] < li[i]) {
     lis[++j] = li[i];
   } else {
-    let idx = binarySearch(0, j, li[i]);
-    lis[idx] = li[i]
+    let idx = binarySearch(lis, 0, j, li[i]);
+    lis[idx] = li[i];
   }
   i++;
 }
-console.log(lis.length)
+console.log(lis.length);
