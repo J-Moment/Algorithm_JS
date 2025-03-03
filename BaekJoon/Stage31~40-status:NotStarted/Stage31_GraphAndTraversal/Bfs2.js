ab9f260d4482ccaa2041acd0655b8edf263ba8e5
SongJunHyuk <83599564+FE-Nerdy@users.noreply.github.com>
2025-03-03 18:30:56 +0900

    Feat: solve "알고리즘 수업 - 너비 우선 탐색 2" problem on BaekJoon
    Bfs로 노드 방문순서 구현

---
+++ BaekJoon/Stage31~40-status:NotStarted/Stage31_GraphAndTraversal/Bfs2.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const filepath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filepath).toString().trim().split('\n');
+
+const [N, M, R] = input[0].split(' ').map(Number);
+const graph = Array.from({ length: N + 1 }, () => []);
+const visited = Array(N + 1).fill(0);
+let order = 1;
+
+for (let i = 1; i <= M; i++) {
+    const [u, v] = input[i].split(' ').map(Number);
+    graph[u].push(v);
+    graph[v].push(u);
+}
+
+for (let i = 1; i <= N; i++) {
+    graph[i].sort((a, b) => b - a);
+}
+
+function bfs(start) {
+    const queue = [start];
+    visited[start] = order++;
+
+    while (queue.length > 0) {
+        const node = queue.shift();
+
+        for (const next of graph[node]) {
+            if (!visited[next]) {
+                visited[next] = order++;
+                queue.push(next);
+            }
+        }
+    }
+}
+
+bfs(R);
+
+console.log(visited.slice(1).join('\n'));
