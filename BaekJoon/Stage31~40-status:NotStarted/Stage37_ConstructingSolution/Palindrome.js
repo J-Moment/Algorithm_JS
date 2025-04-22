aa16e45a83df272bc479d0d935dbdea0c29a0939
SongJunHyuk <83599564+FE-Nerdy@users.noreply.github.com>
2025-04-22 23:46:51 +0900

    Feat: solve "팰린드롬 척화비" problem on BaekJoon
    팰린드롬이 아닌 문자열 문제 풀이 구현

---
+++ BaekJoon/Stage31~40-status:NotStarted/Stage37_ConstructingSolution/Palindrome.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+const filepath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filepath).toString().trim().split("\n");
+
+const n = Number(input);
+
+console.log("a".repeat(n));
