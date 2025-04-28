f05b334cb4b00ed97bba43a6b363992846c06469
SongJunHyuk <83599564+FE-Nerdy@users.noreply.github.com>
2025-04-28 23:07:01 +0900

    Fix: solve "영어 시험" problem on BaekJoon
    조건에 맞는 문자열 출력 구현

--- BaekJoon/Stage31~40-status:NotStarted/Stage37_ConstructingSolution/EnglishTest.js
+++ BaekJoon/Stage31~40-status:NotStarted/Stage37_ConstructingSolution/EnglishTest.js
@@ -3,3 +3,4 @@ const filepath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filepath).toString().trim().split("\n");
 
 let [n, s] = [input[0], input[1]]
+console.log(s.repeat(Number(n)));
