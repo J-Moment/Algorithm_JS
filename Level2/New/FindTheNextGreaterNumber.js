c380af1e76698555c95857993e912f9c1d1576da
SongJunHyuk <83599564+J-Moment@users.noreply.github.com>
2025-08-24 23:54:55 +0900

    Feat: 뒤에 있는 큰 수 찾기
    스택과 반복을 활용하여 문제 풀이 구현

---
+++ Level2/New/FindTheNextGreaterNumber.js
@@ -0,0 +1,13 @@
+function solution(numbers) {
+  let answer = new Array(numbers.length).fill(-1);
+  let stack = [];
+  for (let i = 0; i < numbers.length; i++) {
+    while (numbers[i] > numbers[stack.at(-1)]) {
+      const index = stack.pop();
+      answer[index] = numbers[i];
+    }
+
+    stack.push(i);
+  }
+  return answer;
+}
