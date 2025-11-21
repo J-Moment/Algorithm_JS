78e85cfc232c75a5ce5d1c9f1a04a0ea1a962707
SongJunHyuk <83599564+J-Moment@users.noreply.github.com>
2025-11-21 23:37:59 +0900

    feat: CheckIfObjectInstanceOfClass 구현

---
+++ LeetCode/JavascriptPractice/CheckIfObjectInstanceOfClass.js
@@ -0,0 +1,16 @@
+/**
+ * @param {*} obj
+ * @param {*} classFunction
+ * @return {boolean}
+ */
+var checkIfInstanceOf = function (obj, classFunction) {
+  if (typeof classFunction !== "function") return false;
+
+  if (obj === null || obj === undefined) return false;
+
+  return obj instanceof classFunction || Object(obj) instanceof classFunction;
+};
+
+/**
+ * checkIfInstanceOf(new Date(), Date); // true
+ */
